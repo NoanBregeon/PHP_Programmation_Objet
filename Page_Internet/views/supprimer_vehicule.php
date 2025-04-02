dc39b7807f10db7fba926c6868db60cfe7379e6c
Furie-Nocturne-20 <furienocturne2.0@gmail.com>
2025-04-02 14:51:15 +0200

    Update supprimer_vehicule.php

--- Page_Internet/views/supprimer_vehicule.php
+++ Page_Internet/views/supprimer_vehicule.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'controllers/VehiculeController.php';
+require_once '../controllers/VehiculeController.php';
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
